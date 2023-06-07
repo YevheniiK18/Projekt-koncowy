1d082279838bcb4a47ba455e28b1c6d2653fbb6a
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-07 17:34:50 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def write_xml_file(data, file_path):
+    root = ET.Element("root")
+    root.append(data)
+    tree = ET.ElementTree(root)
+    tree.write(file_path, encoding='utf-8', xml_declaration=True)
+    print("Dane zapisane do pliku XML.")
+
+write_xml_file(xml_data, output_file)
