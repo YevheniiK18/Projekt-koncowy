5911e291ff3718a338f5b02bd3b1a5a5098c2107
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 18:21:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import argparse
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_file', help='Input file path')
-    parser.add_argument('output_file', help='Output file path')
-    return parser.parse_args()
+    parser.add_argument('input_file', help='Ścieżka do pliku wejściowego')
+    parser.add_argument('output_file', help='Ścieżka do pliku wyjściowego')
+    args = parser.parse_args()
+    return args.input_file, args.output_file
+
+input_file, output_file = parse_arguments()
+print(f"Plik wejściowy: {input_file}")
+print(f"Plik wyjściowy: {output_file}")
