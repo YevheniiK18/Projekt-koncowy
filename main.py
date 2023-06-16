3186765532e09caeed028f120cbbee06b24823c4
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 16:50:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def write_json_file(data, file_path):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)
-    print("Data saved to JSON file.")
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+        print("Dane zapisane do pliku JSON.")
+    except IOError as e:
+        print(f"Błąd zapisu do pliku JSON: {e}")
 
 write_json_file(json_data, output_file)
