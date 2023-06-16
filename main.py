53b6aace736d4336783cf48cf39a4e5e6cef9db8
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 15:07:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import json
-
-def load_json(file_path):
+def read_json_file(file_path):
     with open(file_path, 'r') as file:
         try:
             data = json.load(file)
             return data
         except json.JSONDecodeError as e:
-            print(f'Błąd parsowania pliku JSON: {e}')
+            print(f"Error decoding JSON file: {e}")
             return None
 
-file_path = 'plik.json'
-data = load_json(file_path)
-if data:
-    
-    print(data)
+json_data = read_json_file(input_file)
+if json_data is None:
+    sys.exit(1)
