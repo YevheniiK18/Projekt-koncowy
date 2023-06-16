dcd178f22c88490b0d7b87d8dc4e4e62f9276b39
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 17:49:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
+def check_syntax(file_path):
+    try:
+        compile(open(file_path, "rb").read(), file_path, 'exec')
+        return True
+    except SyntaxError:
+        return False
+
 def load_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
+if __name__ == '__main__':
+
+    main_file = __file__
+    if not check_syntax(main_file):
+        print(f"Błąd składni w pliku: {main_file}")
+        sys.exit(1)
+
+
+    imported_modules = [argparse, json, yaml, ET]
+    for module in imported_modules:
+        module_file = module.__file__ if hasattr(module, '__file__') else None
+        if module_file and not check_syntax(module_file):
+            print(f"Błąd składni w pliku: {module_file}")
+            sys.exit(1)
+
+
+    if len(sys.argv) > 1:
+        run_from_command_line()
+    else:
+        run_with_ui()
