76828d7a4a0722ac43d95fd26844c164bd2c6ba4
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 17:20:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def load_xml(file_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
     return root
+
+def save_xml(data, file_path):
+    tree = ET.ElementTree(data)
+    tree.write(file_path)
