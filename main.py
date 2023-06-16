641b6bbc46a49159ceaffdce93e601a842f0faac
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 18:23:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
-def check_syntax(file_path):
+def read_xml_file(file_path):
     try:
-        compile(open(file_path, "rb").read(), file_path, 'exec')
-        return True
-    except SyntaxError:
-        return False
-    
-def load_xml(file_path):
-    tree = ET.parse(file_path)
-    root = tree.getroot()
-    return root
-
-
-main_file = __file__
-    if not check_syntax(main_file):
-        print(f"Błąd składni w pliku: {main_file}")
-        sys.exit(1)
-
-
-    imported_modules = [argparse, json, yaml, ET]
-    for module in imported_modules:
-        module_file = module.__file__ if hasattr(module, '__file__') else None
-        if module_file and not check_syntax(module_file):
-            print(f"Błąd składni w pliku: {module_file}")
-            sys.exit(1)
-
-
-    if len(sys.argv) > 1:
-        run_from_command_line()
-    else:
-        run_with_ui()
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        return root
+    except ET.ParseError as e:
+        print(f"Błąd parsowania pliku XML: {e}")
+        return None
