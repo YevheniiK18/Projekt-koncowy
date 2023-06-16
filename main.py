b8f1dbdc12ca9b2ffef05415639b24c7aa6489a0
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 17:18:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def read_json_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-            return data
-    except (json.JSONDecodeError, FileNotFoundError) as e:
-        print(f"Błąd odczytu pliku JSON: {e}")
-        return None
+ef load_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
 
-json_data = read_json_file(input_file)
-if json_data is None:
-    sys.exit(1)
