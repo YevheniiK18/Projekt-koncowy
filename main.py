69bba2e2ccbcabbc535593fe11f3be95d7a4d40b
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 14:44:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import tkinter as tk
 
 def on_button_click():
-    # Obsługa kliknięcia przycisku
+
     print('Kliknięto przycisk')
 
 root = tk.Tk()
 
-# Dodanie elementów UI
+
 button = tk.Button(root, text='Kliknij mnie', command=on_button_click)
 button.pack()
 
-# Uruchomienie głównej pętli UI
 root.mainloop()
