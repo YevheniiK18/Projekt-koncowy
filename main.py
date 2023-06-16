27c8aa6ad832a2f7f7aebb70c56f3628f8feeb41
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 14:44:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,20 @@ import asyncio
 import threading
 
 def load_file_async(file_path):
-    # Symulacja operacji wczytywania pliku
-    # Można użyć wcześniej przedstawionych funkcji wczytywania
+ 
     data = {'klucz': 'wartość'}
     return data
 
 def save_file_async(data, file_path):
-    # Symulacja operacji zapisu pliku
-    # Można użyć wcześniej przedstawionych funkcji zapisu
+
     print(f'Zapisano dane do pliku: {file_path}')
 
 async def async_file_operations(file_path):
     loop = asyncio.get_event_loop()
 
-    # Wczytywanie pliku asynchronicznie
     load_task = loop.run_in_executor(None, load_file_async, file_path)
     data = await load_task
 
-    # Zapis pliku asynchronicznie
     save_task = loop.run_in_executor(None, save_file_async, data, file_path)
     await save_task
 
