7f1e3e14332369b655eae0a6339fb643e95262cd
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 17:21:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-from PyQt5.QtCore import Qt, QThread, pyqtSignal
+def convert_file(input_file, output_file):
+    if input_file.endswith('.json'):
+        data = load_json(input_file)
+        if output_file.endswith('.yml') or output_file.endswith('.yaml'):
+            save_yaml(data, output_file)
+        elif output_file.endswith('.xml'):
+            save_xml(data, output_file)
+    elif input_file.endswith('.yml') or input_file.endswith('.yaml'):
+        data = load_yaml(input_file)
+        if output_file.endswith('.json'):
+            save_json(data, output_file)
+        elif output_file.endswith('.xml'):
+            save_xml(data, output_file)
+    elif input_file.endswith('.xml'):
+        data = load_xml(input_file)
+        if output_file.endswith('.json'):
+            save_json(data, output_file)
+        elif output_file.endswith('.yml') or output_file.endswith('.yaml'):
+            save_yaml(data, output_file)
+    else:
+        raise ValueError('Invalid input file format.')
 
-class ConvertThread(QThread):
-    conversion_finished = pyqtSignal()
+def run_with_ui():
+    app = QApplication(sys.argv)
+    window = ConverterWindow()
+    window.show()
+    sys.exit(app.exec())
 
-    def __init__(self, input_file, output_file):
-        super().__init__()
-        self.input_file = input_file
-        self.output_file = output_file
+def run_from_command_line():
+    args = parse_arguments()
+    convert_file(args.input_file, args.output_file)
 
-    def run(self):
-        # Wykonaj odpowiednie zadanie konwersji (Task2-Task7) z kodu powyżej
-        # Na końcu, emituj sygnał conversion_finished
-        self.conversion_finished.emit()
-
-    def convert_data(self):
-        thread = ConvertThread(input_file, output_file)
-        thread.conversion_finished.connect(self.on_conversion_finished)
-        thread.start()
-
-    def on_conversion_finished(self):
-        self.show_message_box("Conversion finished.")
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        run_from_command_line()
+    else:
+        run_with_ui()
