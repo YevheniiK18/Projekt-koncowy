76700a461f1b33283d84dcae29b6485bf2c9bb30
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 16:49:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='File Converter')
-parser.add_argument('input_file', help='Input file path')
-parser.add_argument('output_file', help='Output file path')
+parser = argparse.ArgumentParser(description='Konwerter danych')
+parser.add_argument('input_file', help='Ścieżka do pliku wejściowego')
+parser.add_argument('output_file', help='Ścieżka do pliku wyjściowego')
 args = parser.parse_args()
 
 input_file = args.input_file
