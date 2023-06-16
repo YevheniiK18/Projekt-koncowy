6c090241aed8564f77000622f2f5762da0e82a2a
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 16:51:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def write_xml_file(data, file_path):
-    root = ET.Element("root")
-    root.append(data)
-    tree = ET.ElementTree(root)
-    tree.write(file_path, encoding='utf-8', xml_declaration=True)
-    print("Data saved to XML file.")
+    try:
+        root = ET.Element("root")
+        root.append(data)
+        tree = ET.ElementTree(root)
+        tree.write(file_path, encoding='utf-8', xml_declaration=True)
+        print("Dane zapisane do pliku XML.")
+    except IOError as e:
+        print(f"Błąd zapisu do pliku XML: {e}")
 
 write_xml_file(xml_data, output_file)
