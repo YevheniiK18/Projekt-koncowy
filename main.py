5425a9a0cbabbd5c76e6702ab6d24b7bf632da29
YevheniiK18 <129311514+YevheniiK18@users.noreply.github.com>
2023-06-16 17:55:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def check_syntax(file_path):
+    try:
+        compile(open(file_path, "rb").read(), file_path, 'exec')
+        return True
+    except SyntaxError:
+        return False
+    
 def read_xml_file(file_path):
     try:
         tree = ET.parse(file_path)
@@ -10,3 +17,22 @@ def read_xml_file(file_path):
 xml_data = read_xml_file(input_file)
 if xml_data is None:
     sys.exit(1)
+
+main_file = __file__
+    if not check_syntax(main_file):
+        print(f"Błąd składni w pliku: {main_file}")
+        sys.exit(1)
+
+
+    imported_modules = [argparse, json, yaml, ET]
+    for module in imported_modules:
+        module_file = module.__file__ if hasattr(module, '__file__') else None
+        if module_file and not check_syntax(module_file):
+            print(f"Błąd składni w pliku: {module_file}")
+            sys.exit(1)
+
+
+    if len(sys.argv) > 1:
+        run_from_command_line()
+    else:
+        run_with_ui()
